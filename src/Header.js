f6326ecdfd4f6d3bd496e7332aaa7937478c92b2
garrettmac <garrettmacmac@gmail.com>
2019-02-04 12:41:10 -0800

    Initial commit from Create React App

---
+++ src/Header.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+       <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+ 
+        </header>
+       
+      </div>
+    );
+  }
+}
+
+export default App;
